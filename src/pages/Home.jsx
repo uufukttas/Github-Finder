7b320c91c417eca8f46d5440632eb13da8568d17
uufukttas <uufukttas@hotmail.com>
2022-02-14 09:40:43 +0300

    Github users fetch action added.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import UserResults from "../components/users/UserResults";
 
 function Home() {
   return (
     <div>
-      <h1 className="text-6xl">Welcome</h1>
+      <UserResults/>
     </div>
   );
 }
