71b2c62b0e09ea08e2ccc05ece9c5c9d63167483
uufukttas <uufukttas@hotmail.com>
2022-02-26 23:23:58 +0300

    Search Component Added.

---
+++ src/components/users/UserSearch.jsx
@@ -0,0 +1,53 @@
+import { useState, useContext } from "react";
+import GithubContext from "../../Context/Github/GithubContext";
+
+function UserSearch() {
+  const [text, setText] = useState("");
+  const {users} = useContext(GithubContext);
+
+  const handleChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (text === "") {
+      alert("Please enter something");
+    } else {
+      // @todo search
+      setText("");
+    }
+  };
+  return (
+    <div className="mb-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2">
+      <div>
+        <form onSubmit={handleSubmit}>
+          <div className="form-control">
+            <div className="relative">
+              <input
+                type="text"
+                className="input input-lg w-full bg-gray-200 pr-40 text-black"
+                placeholder="Search"
+                value={text}
+                onChange={handleChange}
+              />
+              <button
+                type="Submit"
+                className="btn btn-log absolute top-0 right-0 h-full w-36 rounded-l-none"
+              >
+                Go
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+      {users.length > 0 && (
+        <div>
+          <button className="btn btn-ghost btn-lg">Clear</button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default UserSearch;
